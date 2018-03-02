5e778c06c1ec14060e5e3eef3c80fbfa927396da
Ivson Santos <santosivson@hotmail.com>
2018-03-01 21:43:21 -0300

    funcionando

---
+++ src/main/java/com/ivson/festa/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package com.ivson.festa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ivson.festa.model.Convidado;
+
+public interface ConvidadoRepository extends JpaRepository<Convidado, Long>{
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+--cria este arquivo para inserir alguns dados para a base já ter informações e testar o metodo listar
+insert into convidado (nome) values ('AAAAA');
+insert into convidado (nome) values ('BBBBB');
